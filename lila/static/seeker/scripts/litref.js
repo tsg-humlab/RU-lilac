dc9b9c20380a29bd19f9d38ae6314ab808831e6c
Erwin Komen <erwin_komen@sil.org>
2026-09-10 14:52:04 +0200

    project update

---
+++ lila/static/seeker/scripts/litref.js
@@ -0,0 +1,21 @@
+var litref_006 = {
+  "key": "VJSAKU7R",
+  "version": 2,
+  "itemType": "book",
+  "title": "Les canons des conciles M\u00e9rovingiens (VIe-VIIe si\u00e8cles)",
+  "creators": [
+    { "creatorType": "editor", "firstName": "Jean", "lastName": "Gaudemet" },
+    { "creatorType": "editor", "firstName": "Brigitte", "lastName": "Basdevant" }],
+  "abstractNote": "", "series": "", "seriesNumber": "", "volume": "", "numberOfVolumes": "", "edition": "",
+
+  "place": "Paris", "publisher": "",
+  "date": "1989",
+
+  "numPages": "", "language": "", "ISBN": "",
+  "shortTitle": "Les canons des conciles",
+  "url": "", "accessDate": "", "archive": "", "archiveLocation": "", "libraryCatalog": "", "callNumber": "", "rights": "",
+  "extra": "Series Title: Sources Chr\u00e9tiennes 353",
+  "tags": [], "collections": [], "relations": {},
+  "dateAdded": "2022-10-12T11:39:52Z",
+  "dateModified": "2022-10-12T11:39:52Z"
+}
